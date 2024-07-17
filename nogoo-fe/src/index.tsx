eee6911d74c2cdb661be39478b5e9ae6d37d5192
KANGYONGSU23 <edc20203@gmail.com>
2024-07-17 09:54:43 +0900

    셑

--- nogoo-fe/src/index.tsx
+++ nogoo-fe/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./global.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
