2940e9115172291764da51ff9e1d321128b7397a
timdind <no3232164@dsm.hs.kr>
2024-07-17 11:37:31 +0900

    header 컴포넌트 추가

--- nogoo-fe/src/components/header.tsx
+++ nogoo-fe/src/components/header.tsx
@@ -6,7 +6,11 @@ function Header() {
   return (
     <div className="header">
       <_Header>
-        <img src={Logo} alt="Logo" />
+        <_Left_Header>
+          <_Logo src={Logo} />
+          <_Write>사장님</_Write>
+        </_Left_Header>
+        <_Button>+ 새 공고 작성하기</_Button>
       </_Header>
     </div>
   );
@@ -14,6 +18,32 @@ function Header() {
 
 export default Header;
 
+const _Logo = styled.img``;
+
 const _Header = styled.div`
-  background-color: red;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const _Left_Header = styled.div`
+  display: flex;
+  margin-top: 22px;
+  margin-bottom: 22px;
+  margin-left: 10px;
+`;
+
+const _Write = styled.p`
+  font-size: 12px;
+  color: #3a63cd;
+  margin-top: 40px;
+`;
+
+const _Button = styled.button`
+  width: 138px;
+  height: 37px;
+  border: none;
+  background-color: #f7f7f7;
+  color: #3a63cd;
 `;
